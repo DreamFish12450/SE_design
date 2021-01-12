c021a75e7a5c0f99bdd1d78170166e14be070d36
DreamFish12450 <45327920+DreamFish12450@users.noreply.github.com>
2021-01-12 22:24:26 +0800

    QAQdebug

--- src/ws/parkingLotService/servlet/updateParkingTimeServlet.java
+++ src/ws/parkingLotService/servlet/updateParkingTimeServlet.java
@@ -16,26 +16,30 @@ import java.io.IOException;
 
 @WebServlet("/updateParkingTime.do")
 public class updateParkingTimeServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try {
 
             //int parkingId, int location_x , int location_y
             HttpSession session = request.getSession();
-            String parkingId = (String) request.getParameter("parking_id");
-            String location_x =(String) request.getParameter("car_x");
-            String location_y =(String) request.getParameter("car_y");
-            //System.out.println("parkingId:"+parkingId+"location_x:"+location_x+"location_y"+location_y);
+            String parkingId = request.getParameter("parking_id").trim();
+            String location_x = request.getParameter("car_x").trim();
+            String location_y = request.getParameter("car_y").trim();
+            System.out.println("parkingId:"+parkingId+"location_x:"+location_x+"location_y"+location_y);
+//            System.out.println(parkingId);
             UpdateParkingTime.updateParkingTime(Integer.parseInt(parkingId),Integer.parseInt(location_x),Integer.parseInt(location_y));
             //UpdateParkingTime.updateParkingTime(1,1,1);
             response.setCharacterEncoding("utf-8");
             response.setContentType("application/json;charset=utf-8");
             /*返回数据*/
+            String result = new Gson().toJson(parkingId);
+            response.getWriter().write(result);
             request.setAttribute("msg", "停车成功！");
+
             request.getRequestDispatcher("showMessage.jsp").forward(request, response);
         } catch (Exception throwables) {
             throwables.printStackTrace();
